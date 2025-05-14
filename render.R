406341f55bce08738419c4529b2dc1b51a9f6306
Yixian Huang <hhyixian21@gmail.com>
2025-05-14 12:00:01 -0400

    Preprocess and reshape ES data; add render.R file

---
+++ render.R
@@ -0,0 +1,16 @@
+getwd()
+
+# render.R
+library(rmarkdown)
+
+rmarkdown::render(
+  input = "scripts/00_data_preprocessing.Rmd",
+  output_file = "00_data_preprocessing.html",
+  output_dir = "scripts/R_knit_html"
+)
+
+rmarkdown::render(
+  input = "scripts/01_effectsize_calc_aggregate.Rmd",
+  output_file = "01_effectsize_calc_aggregate.html",
+  output_dir = "scripts/R_knit_html"
+)
